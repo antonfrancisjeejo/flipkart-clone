afb234938eff0d0244a9eed3a661f8b6370bf194
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-01-10 23:09:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import './App.css'
-import Home from './Pages/Home/Home'
-import {Route,Routes} from 'react-router-dom'
-import Products from './Pages/Products/Products'
-import ProductDetails from './Pages/ProductDetails/ProductDetails'
-import Cart from './Pages/Cart/Cart'
+import React, { useEffect } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Products from "./Pages/Products/Products";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import Cart from "./Pages/Cart/Cart";
+import supabase from "./supabase";
+import { useDispatch } from "react-redux";
+import { setUser } from "./slices/userSlice";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  const getUser = async () => {
+    const { data, error } = await supabase.auth.getSession();
+    dispatch(setUser(data.session.user));
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div>
-        <Navbar />
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/products' element={<Products />}/>
-        <Route path='/productdetails/:id' element={<ProductDetails />}/>
-        <Route path='/cart' element={<Cart/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/productdetails/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
-        
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
