a723e2f4da8b25b224f9dc4addfaeaafa30f8cc1
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-01-10 23:10:11 +0530

    Initial commit

---
+++ src/slices/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    removeUser: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setUser, removeUser } = userSlice.actions;
+
+export default userSlice.reducer;
