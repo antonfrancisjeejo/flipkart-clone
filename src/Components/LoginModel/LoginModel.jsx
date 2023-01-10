2df0031252b4610e95e23a6a5eb1e4fcd110b182
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-01-10 23:10:56 +0530

    Initial commit

--- src/Components/LoginModel/LoginModel.jsx
+++ src/Components/LoginModel/LoginModel.jsx
@@ -1,32 +1,94 @@
-import React from 'react'
-import './LoginModel.css'
-import {RxCross2} from 'react-icons/rx'
-const LoginModel = ({isopen,setClose}) => {
-  return (isopen)? (
-    <div className="overlay">
-        <div className='LoginModel'>
-            <div className="left">
-                <div className="left-container">
-                <p className="Login-title">
-                    Login
-                </p>
-                <p className="Login-des">
-                Get access to your Orders, Wishlist and Recommendations
-                </p>
-                </div>
-            </div>
-            <div className="right">
-            <input type="email" className="Login-input" placeholder='Enter Email ' required/>
-            <input type="password" className="Login-input" placeholder='Enter Password ' required/>
-            <p className="termsandcon">By continuing, you agree to Flipkart's <span style={{color:'blue'}}>Terms of Use</span> and 
-            <span style={{color:'blue'}}> Privacy Policy.</span> </p>
-            <button className="Login-btn">Login</button>
-            <p className="Login-signup">New to Flipkart? Create an account</p>
-            </div>
-        <div className="close" onClick={()=>setClose(false)}><RxCross2/></div>
-        </div>
-    </div>
-  ):""
-}
+import React, { useState } from "react";
+import "./LoginModel.css";
+import { RxCross2 } from "react-icons/rx";
+import supabase from "../../supabase";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../slices/userSlice";
 
-export default LoginModel
+const LoginModel = ({ isopen, setClose }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginType, setLoginType] = useState(true);
+
+  const dispatch = useDispatch();
+
+  const signup = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    console.log(data, error);
+  };
+
+  const login = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    dispatch(setUser(data.user));
+  };
+
+  return isopen ? (
+    <div className="overlay">
+      <div className="LoginModel">
+        <div className="left">
+          <div className="left-container">
+            <p className="Login-title">Login</p>
+            <p className="Login-des">
+              Get access to your Orders, Wishlist and Recommendations
+            </p>
+          </div>
+        </div>
+        <div className="right">
+          <input
+            type="email"
+            className="Login-input"
+            placeholder="Enter Email "
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="Login-input"
+            placeholder="Enter Password "
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="termsandcon">
+            By continuing, you agree to Flipkart's{" "}
+            <span style={{ color: "blue" }}>Terms of Use</span> and
+            <span style={{ color: "blue" }}> Privacy Policy.</span>{" "}
+          </p>
+          {loginType ? (
+            <button className="Login-btn" onClick={login}>
+              Login
+            </button>
+          ) : (
+            <button className="Login-btn" onClick={signup}>
+              Signup
+            </button>
+          )}
+          {loginType ? (
+            <p className="Login-signup" onClick={() => setLoginType(false)}>
+              New to Flipkart? Create an account
+            </p>
+          ) : (
+            <p className="Login-signup" onClick={() => setLoginType(true)}>
+              Already an user? Login to an account
+            </p>
+          )}
+        </div>
+        <div className="close" onClick={() => setClose(false)}>
+          <RxCross2 />
+        </div>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+};
+
+export default LoginModel;
