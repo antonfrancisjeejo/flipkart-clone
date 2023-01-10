686fbe0307cd700069026887c7dd4d51827f7a32
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-01-10 23:10:39 +0530

    Initial commit

--- src/Components/BannerCarousel/BannerCarousel.jsx
+++ src/Components/BannerCarousel/BannerCarousel.jsx
@@ -1,57 +1,55 @@
-import React from 'react'
-import './BannerCarousel.css'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import { useState } from 'react'
-import {MdOutlineArrowBackIosNew} from 'react-icons/md'
-import {MdArrowForwardIos} from 'react-icons/md'
-const BannerCarousel = ({data}) => {
-    const settings = {
-        dots:false,
-        infinite: true,
-        speed:1000,
-        autoplay:true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplaySpeed:3000,	
-      };
-const Next=(props)=>{
-    console.log(props);
-    const {className,onClick}=props
-return(
- <div className={className} onClick={onClick}  >
-     <MdArrowForwardIos style={{color:"black",fontSize:25,fontWeight:900}}/>
- </div>
-)
-}
-const Prev=(props)=>{
-    const {className,onClick}=props
-return(
-        <div className={className} onClick={onClick}>
-            <MdOutlineArrowBackIosNew style={{color:'black',fontSize:25,fontWeight:900}} />
-        </div>
-       )
-}
+import React from "react";
+import "./BannerCarousel.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useState } from "react";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
+
+const Next = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <MdArrowForwardIos
+        style={{ color: "black", fontSize: 25, fontWeight: 900 }}
+      />
+    </div>
+  );
+};
+const Prev = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <MdOutlineArrowBackIosNew
+        style={{ color: "black", fontSize: 25, fontWeight: 900 }}
+      />
+    </div>
+  );
+};
+
+const BannerCarousel = ({ data }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <div className="BannerCarousel">
-    <Slider 
-    nextArrow={<Next />}
-    prevArrow={<Prev/>}
-    {...settings}
-    >
-        {data.map((item)=>(
-
-        <div key={item.id}> 
-             <img src={item.Imgsrc} alt="" className='BannerCarousel-Image' />
-        </div>
-        
+      <Slider nextArrow={<Next />} prevArrow={<Prev />} {...settings}>
+        {data.map((item) => (
+          <div key={item.id}>
+            <img src={item.Imgsrc} alt="" className="BannerCarousel-Image" />
+          </div>
         ))}
-    </Slider>
-
-
+      </Slider>
     </div>
-  )
-}
+  );
+};
 
-export default BannerCarousel
+export default BannerCarousel;
