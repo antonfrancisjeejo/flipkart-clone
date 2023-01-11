c297f8be6d4bc0dbcc83d1924e6b14dc32005f10
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-01-11 22:45:50 +0530

    Initial commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,39 +1,66 @@
-import React from 'react'
-import BannerCarousel from '../../Components/BannerCarousel/BannerCarousel'
-import CategoryBar from '../../Components/CategoryBar/CategoryBar'
-import LoginModel from '../../Components/LoginModel/LoginModel'
-import ProductCarousel from '../../Components/CategoryCarousel/CategoryCarousel'
-import { CategoryBarData } from '../../Datas'
-import { CarouselData } from '../../Datas'
-import { BestOf } from '../../Datas'
+import React from "react";
+import BannerCarousel from "../../Components/BannerCarousel/BannerCarousel";
+import CategoryBar from "../../Components/CategoryBar/CategoryBar";
+import LoginModel from "../../Components/LoginModel/LoginModel";
+import ProductCarousel from "../../Components/CategoryCarousel/CategoryCarousel";
+import { CategoryBarData } from "../../Datas";
+import { CarouselData } from "../../Datas";
+import { BestOf } from "../../Datas";
 
-import './Home.css'
+import "./Home.css";
+
 const Home = () => {
   return (
     <div className="Home">
       <div className="Home-CategoryContainer">
         <div className="Home-CategoryBar">
-          {
-            CategoryBarData.map((item)=>(
-              <CategoryBar Imgsrc={item.imageSrc} CategoryName={item.category}/>
-            ))
-          }
+          {CategoryBarData.map((item, index) => (
+            <CategoryBar
+              key={index}
+              Imgsrc={item.imageSrc}
+              CategoryName={item.category}
+            />
+          ))}
         </div>
       </div>
       <div className="Home-Container">
-      <div className="Home-Carousel">
-        <BannerCarousel data={CarouselData}/>
-      </div>
+        <div className="Home-Carousel">
+          <BannerCarousel data={CarouselData} />
+        </div>
 
-      <div className="Home-ProductCarousel">
-        <ProductCarousel BgImg={"https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"} Title={"Best of Electronics"} Data={BestOf.Electronics}/>
-        <ProductCarousel BgImg={"https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"} Title={"Beauty,Food,Toys"} Data={BestOf.Electronics}/>
-        <ProductCarousel BgImg={"https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"} Title={"Winter Essentials"} Data={BestOf.Electronics}/>
-        <ProductCarousel BgImg={"https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"} Title={"Wedding & Gifting Specials"} Data={BestOf.Electronics}/>
+        <div className="Home-ProductCarousel">
+          <ProductCarousel
+            BgImg={
+              "https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"
+            }
+            Title={"Best of Electronics"}
+            Data={BestOf.Electronics}
+          />
+          <ProductCarousel
+            BgImg={
+              "https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"
+            }
+            Title={"Beauty,Food,Toys"}
+            Data={BestOf.Electronics}
+          />
+          <ProductCarousel
+            BgImg={
+              "https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"
+            }
+            Title={"Winter Essentials"}
+            Data={BestOf.Electronics}
+          />
+          <ProductCarousel
+            BgImg={
+              "https://rukminim1.flixcart.com/fk-p-flap/278/278/image/7593e7b6640822c1.jpg?q=90"
+            }
+            Title={"Wedding & Gifting Specials"}
+            Data={BestOf.Electronics}
+          />
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
