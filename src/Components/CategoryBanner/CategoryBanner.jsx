af39d6980bbe914abf1561789767b1d107a2cbb6
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-01-18 22:55:37 +0530

    Initial commit

--- src/Components/CategoryBanner/CategoryBanner.jsx
+++ src/Components/CategoryBanner/CategoryBanner.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import './CategoryBanner.css'
-const CategoryBanner = ({ImgSrc,Title,Brand}) => {
+import React from "react";
+import "./CategoryBanner.css";
+const CategoryBanner = ({ ImgSrc, Title, Brand }) => {
   return (
     <div className="CategoryBanner">
-      <img src={ImgSrc} alt="" className='CategoryBanner-img'/>
+      <img src={ImgSrc} alt="" className="CategoryBanner-img" />
       <p className="CategoryBanner-title">
-        {Title.length<25?Title:Title.slice(0,25)+'...'}</p>
+        {Title.length < 25 ? Title : Title.slice(0, 25) + "..."}
+      </p>
       <p className="CategoryBanner-ShopNow">Shop Now!</p>
       <p className="CategoryBanner-Brands">{Brand}</p>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryBanner
+export default CategoryBanner;
